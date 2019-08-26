3c25cc9fe4b7f4a26f62701c076424e1b9856660
Malpp <cpt.ortan@gmail.com>
2019-08-26 17:30:12 -0400

    + Added player camera

---
+++ Assets/Scripts/LockY.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[ExecuteInEditMode] [SaveDuringPlay] [AddComponentMenu("")]
+public class LockY : CinemachineExtension
+{
+    [Tooltip("Lock the camera's Y position to this value")]
+    public float yPosition = 10;
+ 
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            var pos = state.RawPosition;
+            pos.y = yPosition;
+            state.RawPosition = pos;
+        }
+    }
+}
