cee1611015075e28afadc0ee77ff9e41870412fb
Malpp <cpt.ortan@gmail.com>
2019-08-26 16:57:49 -0400

    + Player can move

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 400f; // Amount of force added when the player jumps.
+    [SerializeField] private float runForce = 40f; // Amount of force added when the player jumps.
+
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f; // How much to smooth out the movement
+    [SerializeField] private bool airControl; // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask whatIsGround; // A mask determining what is ground to the character
+    [SerializeField] private Transform groundCheck; // A position marking where to check if the player is grounded.
+
+    const float GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool _grounded; // Whether or not the player is grounded.
+    private new Rigidbody2D _rigidbody2D;
+    private bool _facingRight = true; // For determining which way the player is currently facing.
+    private Vector3 _velocity = Vector3.zero;
+
+    [Header("Events")] [Space] public UnityEvent onLandEvent;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (onLandEvent == null)
+            onLandEvent = new UnityEvent();
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = _grounded;
+        _grounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, GroundedRadius, whatIsGround);
+        foreach (var collider in colliders)
+        {
+            if (collider.gameObject == gameObject) continue;
+            _grounded = true;
+            if (!wasGrounded)
+                onLandEvent.Invoke();
+        }
+    }
+
+
+    public void Move(float move, bool jump)
+    {
+        if (_grounded || airControl)
+        {
+            Vector3 targetVelocity = new Vector2(move * runForce, _rigidbody2D.velocity.y);
+            _rigidbody2D.velocity =
+                Vector3.SmoothDamp(_rigidbody2D.velocity, targetVelocity, ref _velocity, movementSmoothing);
+
+            if (move > 0 && !_facingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && _facingRight)
+            {
+                Flip();
+            }
+        }
+
+        if (_grounded && jump)
+        {
+            _grounded = false;
+            _rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private void FixedUpdate()
+    private CharacterController2D _controller;
+
+    private float _horizontalMove;
+    private bool _jump;
+
+    private void Awake()
     {
-//        if(Ax)
+        _controller = GetComponent<CharacterController2D>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        _horizontalMove =  Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+        }
+    }
+          
+    void FixedUpdate ()
+    {
+        _controller.Move(_horizontalMove * Time.fixedDeltaTime, _jump);
+        _jump = false;
     }
 }
