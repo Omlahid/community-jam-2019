8dc3f499184805eb08470e414e13c21d5ca8becd
Malpp <cpt.ortan@gmail.com>
2019-08-29 10:59:45 -0400

    + Added error message

---
+++ Assets/Scripts/CrashGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrashGame : MonoBehaviour
+{
+    private static GameObject _gameObject;
+
+    private void Awake()
+    {
+        _gameObject = gameObject;
+        _gameObject.SetActive(false);
+    }
+
+    public static void Crash(String message)
+    {
+        _gameObject.SetActive(true);
+        GameObject.FindWithTag("ErrorMessage").GetComponent<Text>().text = message;
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/Illegal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Illegal : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.Space))
+        {
+            CrashGame.Crash("Error moving character!");
+        }
+    }
+}
