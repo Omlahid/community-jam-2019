5b607a64e547284babb0a1c0ce4d6835e49f077d
Malpp <cpt.ortan@gmail.com>
2019-08-31 09:28:23 -0400

    + title screen

---
+++ Assets/Scripts/MoveCloud.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCloud : MonoBehaviour
+{
+    private RectTransform _canvas;
+    private RectTransform _cloud;
+    private Vector3 _startingPosition;
+    public float speed = -4f;
+ 
+    void Start()
+    {
+        _cloud = gameObject.GetComponent<RectTransform>();
+        _canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        _startingPosition = transform.position;
+    }
+ 
+    void FixedUpdate() {
+        transform.Translate(speed, 0f, 0f);
+        if (_cloud.position.x < -_cloud.rect.width - 100)
+            transform.position = new Vector3(_canvas.rect.width + _cloud.rect.width + 100, _canvas.rect.height * 0.5f + Random.Range(-_canvas.rect.height * 0.2f, _canvas.rect.height * 0.4f), _startingPosition.z);
+    }
+}
